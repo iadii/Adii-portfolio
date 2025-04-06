bf13ac1e0e9f4c56ea80ab2b675e4da43f56a144
Aditya Mishra <mishraaditya3456@gmail.com>
2025-04-07 00:57:01 +0530

    added experience feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import './App.css'
-
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Experience from './components/Experience'; 
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
-  
   return (
-    <div className="bg-gray-800 text-white min-h-screen">
+    <div className="bg-black text-white min-h-screen">
       <Navbar />
       <Hero />
       <About />
       <Projects />
+      <Experience /> 
       <Contact />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
